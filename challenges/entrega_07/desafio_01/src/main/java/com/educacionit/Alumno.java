d5cc087e8ac3a00858340186faf550aebb995556
Gonzaa <gonzalocardin@gmail.com>
2024-02-27 02:16:43 -0300

    'clase alumno'

--- challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Alumno.java
+++ challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Alumno.java
@@ -1,5 +1,54 @@
 package com.educacionit;
 
 public class Alumno {
-    private 
+    private String nombre;
+    private String apellido;
+    private Integer nroExamen;
+    private Integer nota; /// númerico>=0 <=10
+
+    public Alumno(String nombre, String apellido, Integer nroExamen, Integer nota) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nroExamen = nroExamen;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getNroExamen() {
+        return nroExamen;
+    }
+
+    public void setNroExamen(Integer nroExamen) {
+        this.nroExamen = nroExamen;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno [nombre=" + nombre + ", apellido=" + apellido + ", nroExamen=" + nroExamen + ", nota=" + nota
+                + "]";
+    }
+    
 }
