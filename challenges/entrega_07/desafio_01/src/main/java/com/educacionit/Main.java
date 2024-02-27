57cae3e58d2ecc33d782c1900acf2126e0ae8fe2
Gonzaa <gonzalocardin@gmail.com>
2024-02-27 03:17:57 -0300

    'Listo'

--- challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
+++ challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
@@ -1,10 +1,19 @@
 package com.educacionit;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Bienvenidos al sistema de control de notas de la escuela primara XYZ");
-       Scanner scan = new Scanner(System.in);
+        System.out.println("Bienvenidos al sistema de control de notas de la escuela primara XYZ");
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Por favor, ingrese la cantidad de alumnos a procesar: ");
+            Integer cantidadAlumos = scan.nextInt();
+            ManejoDeAlumnos sistema = new ManejoDeAlumnos(cantidadAlumos);
+
+            
+        }catch(InputMismatchException e){
+            System.out.println("No ingreso un valor númerico. Por favor ingrese nuevamente.");
+        }
     }
 }
