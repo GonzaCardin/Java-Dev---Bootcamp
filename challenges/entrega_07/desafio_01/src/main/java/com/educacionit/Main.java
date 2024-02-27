434fd5164fa4a2b114f61c17b4f0c8adc68f72f5
Gonzaa <gonzalocardin@gmail.com>
2024-02-27 03:15:32 -0300

    "Falta main"

--- challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
+++ challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
@@ -1,8 +1,10 @@
 package com.educacionit;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        System.out.println("Bienvenidos al sistema de control de notas de la escuela primara XYZ");
-       
+       Scanner scan = new Scanner(System.in);
     }
 }
