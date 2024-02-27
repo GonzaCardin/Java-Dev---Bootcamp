c2715c4b94aae5b2b3b9c43433039ee77675e3c8
Gonzaa <gonzalocardin@gmail.com>
2024-02-27 02:13:52 -0300

    primer commit

---
+++ challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
@@ -0,0 +1,8 @@
+package com.educacionit;
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Bienvenidos al sistema de control de notas de la escuela primara XYZ");
+       
+    }
+}
