1e026b2af7cb95c4f1c49324402a9c5e1cd7ef04
Gonzaa <gonzalocardin@gmail.com>
2024-02-27 03:31:45 -0300

    Listo, se podrian agregar alguna que otra execepciones

--- challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
+++ challenges/entrega_07/desafio_01/src/main/java/com/educacionit/Main.java
@@ -10,11 +10,33 @@ public class Main {
             System.out.print("Por favor, ingrese la cantidad de alumnos a procesar: ");
             Integer cantidadAlumnos = scan.nextInt();
             ManejoDeAlumnos sistema = new ManejoDeAlumnos(cantidadAlumnos);
-            for(int i = 0; i < cantidadAlumnos ; i++){
-                
+            for (int i = 0; i < cantidadAlumnos; i++) {
+                System.out.println("Ingrese Nombre del Alumno: ");
+                String nombre = scan.next();
+                System.out.println("Ingrese Apellido del Alumno: ");
+                String apellido = scan.next();
+                System.out.println("Ingrese Número del Exámen: ");
+                Integer nroExamen = scan.nextInt();
+                System.out.println("Ingrese nota del Exámen: ");
+                Double nota = scan.nextDouble();
+
+                Alumno a = new Alumno(nombre, apellido, nroExamen, nota);
+                sistema.agregarAlumno(a);
             }
-            
-        }catch(InputMismatchException e){
+            System.out.println("Lista de Alumnos: ");
+            sistema.mostrarAlumno();
+
+            System.out.println("Lista de Alumnos Promocionados: ");
+            System.out.println(sistema.alumnosPromocionados());
+
+            System.out.println("Lista de Alumnos Recursantes: ");
+            System.out.println(sistema.alumnosParaRecursar());
+
+            System.out.println("Alumnos con nota más alta: " + sistema.mostrarNotaAlta());
+            System.out.println("Alumnos con nota más baja: " + sistema.mostrarNotaBaja());
+
+
+        } catch (InputMismatchException e) {
             System.out.println("No ingreso un valor númerico. Por favor ingrese nuevamente.");
         }
     }
