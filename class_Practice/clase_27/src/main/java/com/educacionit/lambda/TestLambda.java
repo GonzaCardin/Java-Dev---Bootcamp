be525a165b36626a5e345e44bea85d2b0f8afb56
Gonzaa <gonzalocardin@gmail.com>
2024-02-28 20:38:05 -0300

    testeados F.P

--- class_Practice/clase_27/src/main/java/com/educacionit/lambda/TestLambda.java
+++ class_Practice/clase_27/src/main/java/com/educacionit/lambda/TestLambda.java
@@ -9,5 +9,19 @@ public class TestLambda {
         Test.ejercicio_03();
         Test.ejercicio_04();
         Test.ejercicio_05();
+        Test.ejercicio_06();
+        Test.ejercicio_07();
+        Test.ejercicio_08();
+        Test.ejercicio_09();
+        Test.ejercicio_11();
+        Test.ejercicio_12();
+        Test.ejercicio_13();
+        Test.ejercicio_15();
+        Test.ejercicio_14();
+        Test.ejercicio_15();
+        Test.ejercicio_17();
+        Test.ejercicio_18();
+        
+        
     }
 }
