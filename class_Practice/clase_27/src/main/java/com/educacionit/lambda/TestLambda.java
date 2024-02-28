5124a73dec8b078b86713245794e781b23f385aa
Gonzaa <gonzalocardin@gmail.com>
2024-02-28 20:35:21 -0300

    ready functional programming

---
+++ class_Practice/clase_27/src/main/java/com/educacionit/lambda/TestLambda.java
@@ -0,0 +1,13 @@
+package com.educacionit.lambda;
+
+public class TestLambda {
+    public static void main(String[] args) {
+        Lambdas Test = new Lambdas();
+
+        Test.ejercicio_01();
+        Test.ejercicio_02();
+        Test.ejercicio_03();
+        Test.ejercicio_04();
+        Test.ejercicio_05();
+    }
+}
