378e18e8141e0df52c6dc7123d3dd1adca27ebc7
Gonzaa <gonzalocardin@gmail.com>
2024-03-10 16:54:23 -0300

    Funciona correctamente todo

--- class_Practice/clase_25/src/main/java/com/educacionit/servlet/InicioServlet.java
+++ class_Practice/clase_25/src/main/java/com/educacionit/servlet/InicioServlet.java
@@ -1,12 +1,16 @@
 package com.educacionit.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.educacionit.model.Alumno;
 
 public class InicioServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -24,16 +28,30 @@ public class InicioServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       ///response.getWriter().append("Served at: ").append(request.getContextPath());
-       PrintWriter printWriter  = response.getWriter();
-       printWriter.append(
-       "<html>" +
-       "<body>" +
-       "<h2>Inicio Servlet</h2>" +
-       "</body>" +
-       "</html>");
+        /*
+         * PrintWriter printWriter = response.getWriter();
+         * printWriter.append(
+         * "<html>" +
+         * "<body>" +
+         * "<h2>Inicio Servlet</h2>" +
+         * "</body>" +
+         * "</html>");
+         */
 
+        List<Alumno> alumnos = new ArrayList<>();
+        alumnos.add(new Alumno(1000, "Gonzalo", 22));
+        alumnos.add(new Alumno(1001, "Luis", 25));
+        alumnos.add(new Alumno(1002, "Cristian", 27));
 
+        /// session.getAttribute("nombre atributo");
+        /// session.setAttribute("nombre de atributo", objeto);
+
+        /// Guardo mi lista en la session
+        HttpSession session = request.getSession();
+        session.setAttribute("listaAlumnos", alumnos);
+
+        /// Forward
+        request.getRequestDispatcher("listado_alumnos.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
