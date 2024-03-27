ba4b580b13a2b8b9b8f2d7ddbce4755261553eaa
Gonzaa <gonzalocardin@gmail.com>
2024-03-27 04:12:07 -0300

    Ejercicio 2 terminado, falta testearlo y  ejercicio 3

--- challenges/entrega_08/src/main/java/com/educacionit/manager/DBManager.java
+++ challenges/entrega_08/src/main/java/com/educacionit/manager/DBManager.java
@@ -9,19 +9,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.educacionit.ejercicio_01.Provincia;
+import com.educacionit.exceptions.DBManagerException;
+
 public class DBManager implements Closeable {
     private Connection con;
 
-    public void openConnection() throws SQLException { /// Hacer excepcion propia try-catch
+    public void openConnection() throws DBManagerException { /// Hacer excepcion propia try-catch
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost/argentina", "root", "Jacques");
             System.out.println("Open Connection");
         } catch (SQLException sqle) {
-            sqle.printStackTrace();
+            /// ERROR 1 - Conexion
+            throw new DBManagerException("No fue posible establecer la conexión.", DBManagerException.ERROR_1);
         }
     }
 
-    public void mostrarProvincias() throws SQLException {
+    public void mostrarProvincias() throws SQLException, DBManagerException {
         String query = "select id_provincia,descripcion from provincias";
         try (Statement stmt = con.createStatement(); ResultSet resultSet = stmt.executeQuery(query)) {
             while (resultSet.next()) {
@@ -30,31 +34,104 @@ public class DBManager implements Closeable {
                 System.out.printf("Id = [%d] - Descripcion = [%s]\n", id, descripcion);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            /// ERROR 2 - mostrarProvincias
+            throw new DBManagerException("No fue posible mostrar las provincias", DBManagerException.ERROR_2);
         }
     }
 
-    public void buscarProvinciasArgentinas(String str) throws SQLException {
+    public void buscarProvinciasArgentinas(String str) throws DBManagerException {
         String query = "select id_provincia,descripcion from provincias where descripcion like ?";
-        PreparedStatement stmt = con.prepareStatement(query);
-        stmt.setString(1, "%" + str + "%");
-        ResultSet rs = stmt.executeQuery();
-        while (rs.next()) {
-            int id = rs.getInt("id_provincia");
-            String descripcion = rs.getString("descripcion");
-            System.out.println("ID: " + id + " Descripcion: " + descripcion);
+        try (PreparedStatement stmt = con.prepareStatement(query)) {
+            stmt.setString(1, "%" + str + "%");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id_provincia");
+                String descripcion = rs.getString("descripcion");
+                System.out.printf("id = [%d] - Descripcion = [%s]\n", id, descripcion);
+            }
+        } catch (SQLException e) {
+            /// ERROR 3 - BuscarProvincias
+            throw new DBManagerException("No fue posible buscar la provincia.", DBManagerException.ERROR_3);
         }
 
     }
 
-    public void closeConnection() throws SQLException {
+    public void agregarProvincia(Provincia prov) throws DBManagerException {
+        String query = "insert into provincias(id_provincia, descripcion) values (?,?)";
+        try (PreparedStatement stmt = con.prepareStatement(query)) {
+            stmt.setInt(1, prov.getId());
+            stmt.setString(2, prov.getDescripcion());
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DBManagerException("La provincia " + prov + "no se pudo agregar correctamente.",
+                        DBManagerException.ERROR_4);
+            }
+            System.out.println("La provincia " + prov + " fue agregada con éxito.");
+        } catch (Exception e) {
+            /// ERROR 4 - Agregar provincia
+            throw new DBManagerException("No fue posible agregar la provincia " + prov, DBManagerException.ERROR_4);
+        }
+
+    }
+
+    public void modificarProvincia(Provincia prov) throws DBManagerException {
+        String query = "update provincias set descripcion = ? where id_provincia = ?";
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, prov.getId());
+            stmt.setString(2, prov.getDescripcion());
+
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DBManagerException("No fue posible modificar la provincia " + prov,
+                        DBManagerException.ERROR_5);/// ERROR 5 - Modificar Provincia
+            }
+            System.out.println("La provincia " + prov + " se modificó con éxito.");
+        } catch (Exception e) {
+            throw new DBManagerException("No fue posible modificar la provincia " + prov, DBManagerException.ERROR_5);
+        } finally {
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                System.err.println("No se pudo cerrar el statement. Error: " + e.getMessage());
+            }
+        }
+    }
+
+    public void borrarProvincia(Integer id) throws DBManagerException {
+        String query = "delete from provincias where id_provincia = ?";
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, id);
+
+            int rowsAffected = stmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DBManagerException("No se pudo eliminar a la provincia " + id, DBManagerException.ERROR_6);
+            }
+            System.out.println("La provincia fue eliminada.");
+        } catch (Exception e) {
+            /// ERROR 6 - Borrar provincia
+            throw new DBManagerException("No se pudo eliminar a la provincia " + id, DBManagerException.ERROR_6);
+        } finally {
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                System.err.println("No se pudo cerrar el statement. Error: " + e.getMessage());
+            }
+        }
+    }
+
+    public void closeConnection() throws DBManagerException {
         try {
             if (con != null) {
                 con.close();
                 System.out.println("Close Connection");
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new DBManagerException("No se pudo cerrar la conexion. Error: " + e.getMessage(), e,
+                    DBManagerException.ERROR_7); /// ERORR 7 - Cerrar Conexion
         }
     }
 
@@ -63,7 +140,7 @@ public class DBManager implements Closeable {
         try {
             closeConnection();
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new IOException(e);
         }
     }
 }
--- challenges/entrega_08/src/main/java/com/educacionit/tests/TestCiudadesArgentinas.java
+++ challenges/entrega_08/src/main/java/com/educacionit/tests/TestCiudadesArgentinas.java
@@ -1,17 +1,37 @@
 package com.educacionit.tests;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
+import com.educacionit.ejercicio_01.Provincia;
+import com.educacionit.exceptions.DBManagerException;
 import com.educacionit.manager.DBManager;
 
 public class TestCiudadesArgentinas {
-    @SuppressWarnings("resource")
-    public static void main(String[] args) throws SQLException {
-        DBManager dm = new DBManager();
-        dm.openConnection();
-        dm.mostrarProvincias();
-        dm.buscarProvinciasArgentinas("Buenos");
-        dm.buscarProvinciasArgentinas("San");
-        dm.closeConnection();
+
+    public static void main(String[] args) throws SQLException, DBManagerException {
+        try (DBManager dm = new DBManager()) {
+            dm.openConnection();
+            dm.mostrarProvincias();
+            System.out.println("--------------------------------");
+            dm.buscarProvinciasArgentinas("Buenos");
+            System.out.println("--------------------------------");
+            dm.buscarProvinciasArgentinas("Santa");
+            System.out.println("--------------------------------");
+
+            Provincia prov = new Provincia(26, "Star");
+            dm.agregarProvincia(prov);
+            System.out.println("---------------------------------");
+
+            Provincia provModif = new Provincia(20, "Santa Fe De La Vera Cruz");
+            dm.modificarProvincia(provModif);
+            System.out.println("--------------------------------");
+
+            dm.borrarProvincia(24);
+
+            dm.closeConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
