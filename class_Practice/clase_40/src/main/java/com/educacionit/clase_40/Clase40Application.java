df9ad03032d6bdaf8210b1468b7032be0cc70496
Gonzalo <gonzalocardin@gmail.com>
2024-04-17 19:26:47 -0300

    --- Clase 40: Spring Boot ----

---
+++ class_Practice/clase_40/src/main/java/com/educacionit/clase_40/Clase40Application.java
@@ -0,0 +1,13 @@
+package com.educacionit.clase_40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase40Application.class, args);
+	}
+
+}
