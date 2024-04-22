c7f48d59ec4ec38b3de25df6e086c8db671d54d8
Gonzalo <gonzalocardin@gmail.com>
2024-04-22 19:31:49 -0300

    --- PAUSE ---

--- class_Practice/clase_40/src/main/java/com/educacionit/controller/AlumnoController.java
+++ class_Practice/clase_40/src/main/java/com/educacionit/controller/AlumnoController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.educacionit.model.Alumno;
 
 @Controller
-@RequestMapping("http://localhost:8080/alumnos")
+@RequestMapping("/alumnos")
 public class AlumnoController {
     /// http://localhost:8080/alumnos/metodo1
     @RequestMapping(value = "/metodo1", method = RequestMethod.GET)
@@ -26,6 +27,18 @@ public class AlumnoController {
         model.setViewName("vista2");
 
         return model;
+    }
+    
+    /// http://localhost:8080/alumnos/metodo3?legajoAlumno=555&nombre=nombreAlumno
+    @GetMapping("/metodo3")
+    public ModelAndView metodo3(@RequestParam(name = "legajo") Integer legajoAlumno, @RequestParam(name = "nombre") String nombreAlumno){
+        ModelAndView mv = new ModelAndView();
+        
+        Alumno alumnoCreado = new Alumno(legajoAlumno, nombreAlumno); 
+        mv.addObject("unAlumno", alumnoCreado);
+        mv.setViewName("vista2");
+        
+        return null;
     }
 
 }
