ffd9861c8cb115ecf561983d5d8971c45a8ff5b9
Gonzalo <gonzalocardin@gmail.com>
2024-05-23 21:53:44 -0300

    ..

--- class_Practice/clase_44/src/main/java/com/educacionit/db/DatabaseConnection.java
+++
@@ -1,9 +0,0 @@
-package com.educacionit.db;
-
-import java.sql.SQLException;
-
-import com.educacionit.db.model.User;
-
-public interface DatabaseConnection {
-    User getUserById(int userId) throws SQLException;
-}
