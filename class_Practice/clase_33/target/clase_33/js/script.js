29385c1c0147b641a74b48923e4a274387b5e493
Gonzalo <gonzalocardin@gmail.com>
2024-05-23 23:20:17 -0300

    Anda el alta ( Servlet probado, vamooos!!!)

---
+++ class_Practice/clase_33/target/clase_33/js/script.js
@@ -0,0 +1,14 @@
+function guardarEdicion(){
+    var legajo  = document.getElementById("legajoAlta").value;
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value; /// sin el value -> Edad: <input type="text" id="edad"><br>
+
+    var xhr = new XMLHttpRequest();
+
+    ///Iniicializar el xhr
+    xhr.open("POST","GuardarAlumnoServlet");
+    // param1=value1&param2=value2&paramX=valueX
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xhr.send("legajo="+legajo+"&nombre="+nombre+"&edad="+edad);
+
+}
