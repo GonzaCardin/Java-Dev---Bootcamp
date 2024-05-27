93aa9dac5c53f217529ba72d15e1867a52c3d0f9
Gonzalo <gonzalocardin@gmail.com>
2024-05-27 11:01:49 -0300

    ...

--- class_Practice/clase_40/src/main/java/com/educacionit/clase_40/repository/impl/AlumnoRepositoryImpl.java
+++ class_Practice/clase_40/src/main/java/com/educacionit/clase_40/repository/impl/AlumnoRepositoryImpl.java
@@ -1,31 +1,37 @@
 package com.educacionit.clase_40.repository.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-import com.educacionit.clase_40.dao.AlumnoDAO;
-import com.educacionit.clase_40.dao.impl.AlumnoDAOImpl;
 import com.educacionit.clase_40.model.Alumno;
 import com.educacionit.clase_40.repository.AlumnoRepository;
 
 
 @Repository("alumnoRepository")
+@Scope("prototype")
 public class AlumnoRepositoryImpl implements AlumnoRepository {
-	
+	private static List<Alumno> alumnos = new ArrayList<>();
+
+    public AlumnoRepositoryImpl() {
+		super();
+		alumnos.add(new Alumno(1234, "Nombre1234",27));
+		alumnos.add(new Alumno(5678, "Nombre5678",30));
+
+    }
 
 	@Override
 	public List<Alumno> getAll() {
-		AlumnoDAO alumnoDao = new AlumnoDAOImpl();
-		
-		return alumnoDao.getAll();
+		return alumnos;
 	}
 
 	@Override
 	public Alumno save(Alumno alumno) {
-		AlumnoDAO alumnoDao = new AlumnoDAOImpl();
-		
-		return alumnoDao.save(alumno);
+		// AlumnoDAO alumnoDao = new AlumnoDAOImpl();
+		alumnos.add(alumno);
+		return alumno;
 	}
 
 }
