900e0c9c4ff4d2b08eba9e7a575dfa9a961344e7
Gonzalo <gonzalocardin@gmail.com>
2024-05-27 14:54:46 -0300

    Clase 41 -> Inicio, no se toca más nada que asi anda correcto

--- class_Practice/clase_40/src/main/java/com/educacionit/clase_40/repository/impl/AlumnoRepositoryImpl.java
+++ class_Practice/clase_40/src/main/java/com/educacionit/clase_40/repository/impl/AlumnoRepositoryImpl.java
@@ -1,11 +1,10 @@
 package com.educacionit.clase_40.repository.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.educacionit.clase_40.dao.AlumnoDAO;
-import com.educacionit.clase_40.dao.impl.AlumnoDAOImpl;
 import com.educacionit.clase_40.model.Alumno;
 import com.educacionit.clase_40.repository.AlumnoRepository;
 
@@ -13,19 +12,22 @@ import com.educacionit.clase_40.repository.AlumnoRepository;
 @Repository("alumnoRepository")
 public class AlumnoRepositoryImpl implements AlumnoRepository {
 	
+	private static List<Alumno>  alumnos = new ArrayList<>();
+
+    public AlumnoRepositoryImpl() {
+		alumnos.add(new Alumno(1234, "Eduardo",27));
+		alumnos.add(new Alumno(5678, "Cristian",30));
+    }
 
 	@Override
-	public List<Alumno> getAll() {
-		AlumnoDAO alumnoDao = new AlumnoDAOImpl();
-		
-		return alumnoDao.getAll();
+	public List<Alumno> getAll() {	
+		return alumnos;
 	}
 
 	@Override
 	public Alumno save(Alumno alumno) {
-		AlumnoDAO alumnoDao = new AlumnoDAOImpl();
-		
-		return alumnoDao.save(alumno);
+		alumnos.add(alumno);
+		return alumno;		
 	}
 
 }
